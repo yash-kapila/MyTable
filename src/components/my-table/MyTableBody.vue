4d4e83b6a8b30b7e2f53ac6a6c9cf18567bca4f4
Yash Kapila <yash.kapila18@gmail.com>
2017-08-27 11:43:27 +0200

    custom template rendering

---
+++ src/components/my-table/MyTableBody.vue
@@ -0,0 +1,33 @@
+<script>
+import Vue from 'vue';
+
+import MyTableConstant from './services/MyTableConstant';
+
+export default {
+  name: 'MyTableBody',
+  data() {
+    return {
+      template: null,
+      defaultCell: '',
+    };
+  },
+  props: ['row', 'col'],
+  render(createElement) {
+    if (this.template) {
+      return this.template();
+    }
+    return createElement('div', '');
+  },
+  created() {
+    if (this.col.template) {
+      this.template = Vue.compile(this.col.template).render;
+    } else {
+      this.template = Vue.compile(MyTableConstant.defaultBodyCellTemplate).render;
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
